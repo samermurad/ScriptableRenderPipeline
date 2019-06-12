81fcc7561e72926bb5ce5b7f167193dd68c101e3
Parashar Krishnamachari <parashar@unity3d.com>
2019-06-12 16:35:34 -0700

    Grant access to the future SpeedTree package so that it can contain the future ShaderGraph node definitions

--- com.unity.shadergraph/Editor/AssemblyInfo.cs
+++ com.unity.shadergraph/Editor/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Unity.ShaderGraph.Editor.Tests")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.Lightweight.Editor")]
 [assembly: InternalsVisibleTo("Unity.RenderPipelines.HighDefinition.Editor")]
+[assembly: InternalsVisibleTo("Unity.SpeedTree.Editor")]
